d2e84259999b2666ec16750da2db2dc53ad71efd
Toyo-Daichi <s1920998@s.tsukuba.ac.jp>
2019-12-05 07:31:45 +0900

    added csv2typhoon_plot.py @2019.12.5

--- csv2typhoon_plot.py
+++ csv2typhoon_plot.py
@@ -17,31 +17,32 @@ class mapping:
 	def __init__(self):
 		pass
 
-	def setup_csv_filelist(self, path, time_list, ft_grd):
+	def setup_csv_filelist(self, path):
 		filelist = []
-		filelist = glob.glob(path + 'table' + '*.csv')
+		filelist = glob.glob(path + '*.csv')
 		return filelist
 
 	def open_csv_filelist(self, csv_filelist):
-		csv_datalist = []
+		csv_datalist = [ []*i for i in range(len(csv_filelist)) ]
+		list_num = list(range(11)) 
 		list_option = ( 'year', 'month', 'day', 'hour(UTC)', 'typhoon number', 'typhoon name', 'rank','latitude', 'longinitude', 'central pressure', 'max wind')
 		for num_file, infile in enumerate(csv_filelist):
-			tmp_data = pd.read_csv(infile, usecols=[0, 10], skiprows=1, names=list_option)
-			print(tmp_data)
-			tmp_list = tmp_data.values.tolist()
-			csv_datalist.extend(tmp_list)
+			tmp_data = pd.read_csv(infile, usecols=list_num, skiprows=1, names=list_option, sep=',')
+			tmp_list = tmp_data['latitude'].values.tolist()
+			csv_datalist[num_file].extend(tmp_list)
 			print('..... Preparating data for ' + str(num_file) + ' ' + str(infile))
-		return trajectory_ondatalist
+		return csv_datalist
 
 	def main_driver(self, indir, typhoon_num):
-		csv_filelist = self.setup_csv_filelist(self, indir)
-		csv_datalist = self.open_csv_filelist(self, csv_filelist)
+		csv_filelist = self.setup_csv_filelist(indir)
+		csv_datalist = self.open_csv_filelist(csv_filelist)
+		print(csv_datalist[0])
 
 if __name__ == "__main__":
 	mapp = mapping()
 
 	#input dir
-	indir = '/work3/daichi/MRI/Data/typhoon_csv'
+	indir = '/work3/daichi/MRI/Data/typhoon_csv/'
 
 	#typhoon target name
 	typhoon_num = [1919, 1921]
